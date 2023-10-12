7492c33da4efd4fb9ede451cfa89600915156715
jakob.deutloff <jakob.deutloff@gmail.com>
2023-10-12 14:53:07 +0200

    Cloudsat from olli and runs from Freddi

--- cloudsat.py
+++ cloudsat.py
@@ -1,14 +1,35 @@
-#%%
-import rioxarray as rxr
-import gdal
+# %%
+from typhon.files import FileSet, CloudSat
+import xarray as xr
 
-# Path to the CloudSat file
-path = '/work/bm1183/m301049/cloudsat/'
+# %%Path to the CloudSat file
+path = "/work/bm1183/m301049/cloudsat/"
 
-# %% open with gdal 
-f1 = gdal.Open(path + 'ice/2014090182520_42155_CS_2C-ICE_GRANULE_P1_R05_E06_F00.hdf')
+# %%
+def collect_cloudsat(start, end):
+    cloudsat_files = FileSet(
+        name="2C-ICE",
+        path="/work/um0878/data/cloudsat/2C-ICE.P1_R05/{year}/{doy}/"
+             "{year}{doy}{hour}{minute}{second}_*.hdf",
+        handler=CloudSat(),
+        # Each file of CloudSat covers exactly 5933 seconds. Since we state it
+        # here, the indexing of files is much faster
+        time_coverage="5933 seconds",
+        # Load only the fields that we need:
+        read_args={
+            "fields": ["ice_water_path"],
+        },
+        max_threads=15,
+    )
 
-# %% open with rasterio - data seems to be missiong 
-f1 = rxr.open_rasterio(filename=path + 'ice/2014090182520_42155_CS_2C-ICE_GRANULE_P1_R05_E06_F00.hdf')
+    print("Collect 2C-ICE...")
+    data = xr.concat(
+        cloudsat_files[start:end],
+        dim="scnline"
+    )
 
+    return data
+# %%
+data = collect_cloudsat("2007-01-01", "2007-01-02")
 
+# %%
