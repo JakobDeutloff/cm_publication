fa5dab181f8352c6b38227b51efe9297dbbfe25f
jakob.deutloff <jakob.deutloff@gmail.com>
2023-12-22 10:52:26 +0100

    Added Comments

--- src/hc_model.py
+++ src/hc_model.py
@@ -7,57 +7,154 @@ import pandas as pd
 
 
 def calc_constants(fluxes_3d):
+    """
+    Calculates constants for the model.
+
+    PARAMETERS:
+    ---------------------------
+    fluxes_3d: xarray.Dataset
+        3D fluxes data.
+
+    RETURNS:
+    ---------------------------
+    albedo_cs: float
+        Clearsky albedo.
+    R_t_cs: float
+        Clearsky R_t."""
+
     albedo_cs = fluxes_3d["albedo_clearsky"].mean()
-    R_t_cs = (
-        fluxes_3d["clearsky_lw_up"].isel(pressure=-1).mean()
-    )
-    SW_in = (
-        fluxes_3d["clearsky_sw_down"]
-        .isel(pressure=-1)
-        .mean()
-        .values
-    )
+    R_t_cs = fluxes_3d["clearsky_lw_up"].isel(pressure=-1).mean()
+    SW_in = fluxes_3d["clearsky_sw_down"].isel(pressure=-1).mean().values
     return albedo_cs, R_t_cs, SW_in
 
 
 def logisic(x, L, x0, k, j):
+    """
+    Logistic function.
+
+    PARAMETERS:
+    ---------------------------
+    x: array-like
+        Input data.
+    L: float
+        Maximum value.
+    x0: float
+        Midpoint.
+    k: float
+        Steepness.
+    j: float
+        Offset.
+
+    RETURNS:
+    ---------------------------
+    y: array-like
+        Output data.
+    """
     return L / (1 + np.exp(-k * (x - x0))) + j
 
 
 def calc_hc_temperature(IWP_bins, lw_vars, atms):
+    """
+    Calculates the cloud top temperature.
+    
+    PARAMETERS:
+    ---------------------------
+    IWP_bins: array-like
+        Bins for IWP.
+    lw_vars: xarray.Dataset
+        Longwave variables.
+    atms: xarray.Dataset
+        Atmosphere data.
+    
+    RETURNS:
+    ---------------------------
+    T_hc_binned: pd.Series
+        Cloud top temperature.
+    """
     T_hc_binned = (
-        lw_vars["h_cloud_temperature"]
-        .groupby_bins(atms["IWP"], IWP_bins)
-        .mean()
+        lw_vars["h_cloud_temperature"].groupby_bins(atms["IWP"], IWP_bins).mean()
     )
     return T_hc_binned
 
 
 def calc_LWP(IWP_bins, atms):
+    """
+    Calculates the liquid water path.
+    
+    PARAMETERS:
+    ---------------------------
+    IWP_bins: array-like
+        Bins for IWP.
+    atms: xarray.Dataset
+        Atmosphere data.
+    
+    RETURNS:
+    ---------------------------
+    LWP_binned: pd.Series
+        Liquid water path.
+    """
     LWP_binned = (
-        atms["LWP"]
-        .where(atms["LWP"] > 1e-6)
-        .groupby_bins(atms["IWP"], IWP_bins)
-        .mean()
+        atms["LWP"].where(atms["LWP"] > 1e-6).groupby_bins(atms["IWP"], IWP_bins).mean()
     )
     return LWP_binned
 
 
 def calc_lc_fraction(IWP_bins, atms):
-    lc_fraction_binned = (
-        atms["lc_fraction"]
-        .groupby_bins(atms["IWP"], IWP_bins)
-        .mean()
-    )
+    """
+    Calculates the low cloud fraction.
+    
+    PARAMETERS:
+    ---------------------------
+    IWP_bins: array-like
+        Bins for IWP.
+    atms: xarray.Dataset
+        Atmosphere data.
+    
+    RETURNS:
+    ---------------------------
+    lc_fraction_binned: pd.Series
+        low cloud fraction.
+    """
+    lc_fraction_binned = atms["lc_fraction"].groupby_bins(atms["IWP"], IWP_bins).mean()
     return lc_fraction_binned
 
 
 def calc_hc_albedo(IWP, alpha_hc_params):
+    """
+    Calculates the high-cloud albedo.
+
+    PARAMETERS:
+    ---------------------------
+    IWP: array-like
+        Input data.
+    alpha_hc_params: tuple
+        Parameters for the logistic function.
+
+    RETURNS:
+    ---------------------------
+    fitted_vals: array-like
+        high cloud albedo.
+    """
     fitted_vals = logisic(np.log10(IWP), *alpha_hc_params, 0)
     return fitted_vals
 
 
 def calc_hc_emissivity(IWP, em_hc_params):
+    """
+    Calculates the high-cloud emissivity.
+    
+    PARAMETERS:
+    ---------------------------
+    IWP: array-like
+        Input data.
+    em_hc_params: tuple
+        Parameters for the logistic function.
+        
+    RETURNS:
+    ---------------------------
+    fitted_vals: array-like
+        high cloud emissivity.
+    """
     fitted_vals = logisic(np.log10(IWP), *em_hc_params, 0)
     return fitted_vals
 
@@ -96,9 +193,28 @@ def hc_lw_cre(em_hc, T_h, R_t, sigma):
 
 
 def run_model(IWP_bins, fluxes_3d, atms, lw_vars, parameters, const_lc_quantities=None):
-
     """
-    Runs the HC Model with given input data and parameters."""
+    Runs the HC Model with given input data and parameters.
+
+    INPUT:
+    ---------------------------
+    IWP_bins: array-like
+        Bins for IWP.
+    fluxes_3d: xarray.Dataset
+        3D fluxes data.
+    atms: xarray.Dataset
+        Atmosphere data.
+    lw_vars: xarray.Dataset
+        Longwave variables.
+    parameters: dict
+        Parameters for the model.
+    const_lc_quantities: dict, optional
+        Constant values for lc quantities.
+
+    RETURNS:
+    ---------------------------
+    results: pd.DataFrame
+        Results of the model."""
 
     IWP_points = (IWP_bins[1:] + IWP_bins[:-1]) / 2
 
@@ -109,7 +225,9 @@ def run_model(IWP_bins, fluxes_3d, atms, lw_vars, parameters, const_lc_quantitie
     T_hc = calc_hc_temperature(IWP_bins, lw_vars, atms)
     LWP = calc_LWP(IWP_bins, atms)
     lc_fraction = calc_lc_fraction(IWP_bins, atms)
-    alpha_t = calc_alpha_t(LWP, lc_fraction, albedo_cs, parameters["alpha_t"], const_lc_quantities)
+    alpha_t = calc_alpha_t(
+        LWP, lc_fraction, albedo_cs, parameters["alpha_t"], const_lc_quantities
+    )
     R_t = calc_R_t(LWP, lc_fraction, R_t_cs, parameters["R_t"], const_lc_quantities)
     alpha_hc = calc_hc_albedo(IWP_points, parameters["alpha_hc"])
     em_hc = calc_hc_emissivity(IWP_points, parameters["em_hc"])
